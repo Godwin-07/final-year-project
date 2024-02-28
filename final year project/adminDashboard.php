6f57073770e7b219a9ef139cb24439ae28f6958a
Godwin <godwinabraham.a@gmail.com>
2024-02-29 03:01:13 +0530

    .

--- final year project/adminDashboard.php
+++ final year project/adminDashboard.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <head>
-
+<link rel="stylesheet" href="adminDashboard.css">
 </head>
 <body>
     <?php include 'nav.php';?>
     <div class="main">
     <div class="side_bar">
-        
+       Menu
+       <button class="" onclick="function"><h5>Schemes</h5></a></button>
+       <button class=""><h5>Applications</h5></button>
+       <button class=""><h5>Manage</h5></button>
     </div>
     <div class="view">
-
+        My page
     </div>
     </div>
 </body>
