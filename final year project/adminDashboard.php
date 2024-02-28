1b9e6f76b0b5754291cc24858c40c4aaa2a86746
Godwin-07 <godwinabraham.a@gmail.com>
2024-02-28 16:17:20 +0530

    .

---
+++ final year project/adminDashboard.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+
+</head>
+<body>
+    <?php include 'nav.php';?>
+    <div class="main">
+    <div class="side_bar">
+        
+    </div>
+    <div class="view">
+
+    </div>
+    </div>
+</body>
+</html>
