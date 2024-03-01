be12ad617a31085ea752796ea5c309b173f3a0ec
Godwin-07 <godwinabraham.a@gmail.com>
2024-03-01 13:58:03 +0530

    .

--- final year project/adminDashboard.php
+++ final year project/adminDashboard.php
@@ -6,18 +6,19 @@
     <?php include 'nav.php';?>
     <div class="main">
     <div class="side_bar">
-        <div class="btn">
-       <h style="
-       font-size:20px;
-       padding:7px 0 0px 10px"><u>
-       Menu</u></h>
-       <button class="menu" onclick=""><h4>Schemes</h4></button><hr>
-       <button class="menu"><h4>Applications</h4></button><hr>
-       <button class="menu"><h4>Manage</h4></button><hr>
-        </div>
+    <div class="btn">
+
+<h style="
+font-size:20px;
+padding:7px 0 0px 10px"><u>
+Menu</u></h>
+<button class="menu" onclick=""><h4>Schemes</h4></button><hr>
+<button class="menu"><h4>Applications</h4></button><hr>
+<button class="menu"><h4>Manage</h4></button><hr>
+ </div>
     </div>
     <div class="view">
-        My Page
+        My Page 
     </div>
     </div>
 </body>
